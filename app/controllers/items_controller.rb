b44355304c0d009b62bd56584dbd59f0549e843c
Xica Felix <xicasfelix@gmail.com>
2023-02-09 17:50:30 -0500

    commit tree ac226a144f8120b4ff0591b164de0c72f7bf3f11

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = find_item
+    render json: item
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+    else
+      item = Item.create(item_params)
+    end
+    render json: item, status: :created
+  end
+
+  private
+
+  def not_found_response(exception)
+    render json: {error: "#{exception.model} not found"}, status: :not_found
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price )
+  end
 end
